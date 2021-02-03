0817d17ae7f22feef8ee5439bcb0ce8a72886381
lenovo <e.orechovich@tut.by>
2021-02-03 17:00:05 +0300

    add tests

--- reminder/tests.py
+++ reminder/tests.py
@@ -1,27 +1,27 @@
 from json import dumps
-
-from django.test import TestCase
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.reverse import reverse
-from rest_framework.test import APITestCase
-
-from reminder.models import MyUser
+from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate, APIClient, RequestsClient
+from reminder.models import MyUser, Event
 
 
 class RestTest(APITestCase):
     def setUp(self):
         self.login = "test_name"
+        self.email = 'ff@ff.ff'
         self.pwd = "test_pwd"
         self.user = MyUser.objects.create_user(
             username=self.login,
-            password=self.pwd
+            password=self.pwd,
+            email=self.email
         )
 
     def test_create_token(self):
         url = reverse("create-token")
         data = {
             "login": self.login,
+            "email": self.email,
             "pwd": self.pwd
         }
         response = self.client.post(
@@ -34,3 +34,95 @@ class RestTest(APITestCase):
             Token.objects.get(user=self.user).__str__(),
             response.json()['token']
         )
+
+    def test_get_event(self):
+        url = reverse("create-token")
+        data = {
+            "login": self.login,
+            "email": self.email,
+            "pwd": self.pwd
+        }
+        response = self.client.post(
+            path=url,
+            data=dumps(data),
+            content_type="application/json"
+        )
+        client = RequestsClient()
+        headers = {'Authorization': 'Token ' + Token.objects.get(user=self.user).key}
+        response = client.get('http://127.0.0.1:8000/event/addevent', headers=headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_add_event(self):
+        url = reverse("create-token")
+        data = {
+            "login": self.login,
+            "email": self.email,
+            "pwd": self.pwd
+        }
+        response = self.client.post(
+            path=url,
+            data=dumps(data),
+            content_type="application/json"
+        )
+        client = RequestsClient()
+
+        headers = {'Authorization': 'Token ' + Token.objects.get(user=self.user).key}
+        data = {
+            "event": 'testevent',
+            "date_event": '2021-02-23',
+            "time_start": '10:00',
+        }
+        response = client.post('http://127.0.0.1:8000/event/addevent', headers=headers, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_your_events(self):
+        url = reverse("create-token")
+        data = {
+            "login": self.login,
+            "email": self.email,
+            "pwd": self.pwd
+        }
+        response = self.client.post(
+            path=url,
+            data=dumps(data),
+            content_type="application/json"
+        )
+        client = RequestsClient()
+        headers = {'Authorization': 'Token ' + Token.objects.get(user=self.user).key}
+        response = client.get('http://127.0.0.1:8000/event/yourevents/', headers=headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_yourmonthevents(self):
+        url = reverse("create-token")
+        data = {
+            "login": self.login,
+            "email": self.email,
+            "pwd": self.pwd
+        }
+        response = self.client.post(
+            path=url,
+            data=dumps(data),
+            content_type="application/json"
+        )
+        client = RequestsClient()
+        headers = {'Authorization': 'Token ' + Token.objects.get(user=self.user).key}
+        response = client.get('http://127.0.0.1:8000/event/yourmonthevents/', headers=headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_holidays(self):
+        url = reverse("create-token")
+        data = {
+            "login": self.login,
+            "email": self.email,
+            "pwd": self.pwd
+        }
+        response = self.client.post(
+            path=url,
+            data=dumps(data),
+            content_type="application/json"
+        )
+        client = RequestsClient()
+        headers = {'Authorization': 'Token ' + Token.objects.get(user=self.user).key}
+        response = client.get('http://127.0.0.1:8000/event/holidays/', headers=headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
--- reminder/urls.py
+++ reminder/urls.py
@@ -4,10 +4,10 @@ from reminder.views import RegisterAPI, CreateEvent, CreateToken, YourEvents, Yo
 urlpatterns = [
     path('register', RegisterAPI.as_view(), name='register'),
     path('create_token', CreateToken.as_view(), name='create-token'),
+    path('loginuser/', LoginUser.as_view()),
     path('rest-auth/', include('rest_auth.urls')),
     path('holidays/', YourHolidays.as_view()),
     path('addevent', CreateEvent.as_view(), name='create-event'),
     path('yourmonthevents/', YourMonthEvents.as_view()),
-    path('loginuser/', LoginUser.as_view()),
     path('yourevents/', YourEvents.as_view()),
 ]
