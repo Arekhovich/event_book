2f2b90950d961b8cd224e92bccb0613d78b497f1
Arekhovich <62569325+Arekhovich@users.noreply.github.com>
2021-03-19 10:30:53 +0300

    Update urls.py

--- reminder/urls.py
+++ reminder/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from reminder.views import RegisterAPI, CreateEvent, CreateToken, YourEvents, YourHolidays, YourMonthEvents, LoginUser
 
 urlpatterns = [
@@ -6,6 +6,7 @@ urlpatterns = [
     path('create_token', CreateToken.as_view(), name='create-token'),
     path('loginuser', LoginUser.as_view()),
     path('rest-auth/', include('rest_auth.urls')),
+    re_path(r'^[-\w]+/'),
     path('holidays', YourHolidays.as_view()),
     path('addevent', CreateEvent.as_view(), name='create-event'),
     path('yourmonthevents', YourMonthEvents.as_view()),
